b9db475a66acd441dcdc86cefe5eaee7f8c9fe76
crimsonprince <kingr4@protonmail.ch>
2019-12-08 21:37:41 +0000

    Added Leaflet + Default home Page
    Signed-off-by: crimsonprince <kingr4@protonmail.ch>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { latLng, tileLayer, rectangle } from 'leaflet';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FYP-Frontend';
+    options = {
+      layers: [
+        tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' }),
+        rectangle([[30.05, -24.95 ], [ 71.95, 44.95 ], [ 71.95, -24.95 ], [30.05, 44.95]]),
+      ],
+      zoom: 4.5,
+      center: latLng(56.17, 9.55)
+    };
 }
