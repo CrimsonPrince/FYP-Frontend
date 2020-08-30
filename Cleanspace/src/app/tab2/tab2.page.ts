9d4c35289451a636f852728ae594bb7ae4435cbe
r4 <kingr4@protonmail.ch>
2020-08-30 15:47:58 +0100

    ngx Leaflet standpoint
    Signed-off-by: r4 <kingr4@protonmail.ch>

--- Cleanspace/src/app/tab2/tab2.page.ts
+++ Cleanspace/src/app/tab2/tab2.page.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
-import * as L from 'leaflet';
+import { tileLayer, latLng, InvalidateSizeOptions, Map } from 'leaflet';
+// import * as L from 'ngx-leaflet';
 
-import 'mapbox-gl';
-import 'mapbox-gl-leaflet';
+// import 'mapbox-gl';
+// import 'mapbox-gl-leaflet';
+// import '@ngi-digital/leaflet.markercluster';
+
 
 @Component({
   selector: 'app-tab2',
@@ -11,6 +14,15 @@ import 'mapbox-gl-leaflet';
 })
 export class Tab2Page {
 
+  options = {
+    layers: [
+      tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+    ],
+    zoom: 5,
+    center: latLng(46.879966, -121.726909)
+  };
+
+  public map: Map = null;
 
   constructor() {}
 
@@ -18,26 +30,41 @@ export class Tab2Page {
     ionViewDidEnter(){
       this.loadMap();
     }
+
+
+  onMapReady(map: Map) {
+    this.map = map;
+  }
+
+  onResized() {
+    if (this.map) {
+      this.map.invalidateSize();
+    }
+  }
    // The below function is added
     loadMap() {
 
-      var token ="pk.eyJ1IjoiY3JpbXNvbnByaW5jZSIsImEiOiJjazQyejZmajAwMmN6M2tsa211bW42bm84In0.ygXb37nll4_Y7PqqQy9JFw"; // replace with your Mapbox API Access token. Create a Mapbox account and find it on https://account.mapbox.com/
+    //   var token ="pk.eyJ1IjoiY3JpbXNvbnByaW5jZSIsImEiOiJjazQyejZmajAwMmN6M2tsa211bW42bm84In0.ygXb37nll4_Y7PqqQy9JFw"; // replace with your Mapbox API Access token. Create a Mapbox account and find it on https://account.mapbox.com/
 
-      var map = L.map('map').setView([38.912753, -77.032194], 15);
-      L.marker([38.912753, -77.032194])
-          .bindPopup("Hello <b>Leaflet GL</b>!<br>Whoa, it works!")
-          .addTo(map)
-          .openPopup();
-      var gl = L.mapboxGL({
-          accessToken: token,
-          style: 'mapbox://styles/mapbox/light-v10'
-      }).addTo(map);
+    //   var map = L.map('map').setView([38.912753, -77.032194], 15);
+    //   L.marker([38.912753, -77.032194])
+    //       .bindPopup("Hello <b>Leaflet GL</b>!<br>Whoa, it works!")
+    //       .addTo(map)
+    //       .openPopup();
+    //   var gl = L.mapboxGL({
+    //       accessToken: token,
+    //       style: 'mapbox://styles/mapbox/light-v10'
+    //   }).addTo(map);
 
-    // this.map = new Map("mapId").setView([17.3850,78.4867], 13);
+    //   var markers = L.markerClusterGroup();
+    //   // markers.addLayer(L.marker());
+    //   map.addLayer(markers);
 
-    // tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-    //   { attribution: 'Map data © <a href="https://www.openstreetmap.org/"> OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY- SA</a>'}).addTo(this.map);
-    // }
+    // // this.map = new Map("mapId").setView([17.3850,78.4867], 13);
+
+    // // tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    // //   { attribution: 'Map data © <a href="https://www.openstreetmap.org/"> OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY- SA</a>'}).addTo(this.map);
+    // // }
     }
 
 }
