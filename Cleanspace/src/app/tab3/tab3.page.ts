c75b2c8095311a492a1f7a5bf8b69fbad9dc61f4
r4 <kingr4@protonmail.ch>
2020-09-07 00:05:39 +0100

    Added Country Field
    Signed-off-by: r4 <kingr4@protonmail.ch>

--- Cleanspace/src/app/tab3/tab3.page.ts
+++ Cleanspace/src/app/tab3/tab3.page.ts
@@ -1,15 +1,36 @@
 import { Component } from '@angular/core';
 import { Geolocation} from '@capacitor/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { elementEventFullName } from '@angular/compiler/src/view_compiler/view_compiler';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
+@Injectable()
 export class Tab3Page {
 
-  constructor() {
-    // this.getCountries();
+  constructor(private http: HttpClient) {
+    this.loadCountries();
+  }
+
+  private url: string;
+  private countries: any[] = [];
+
+  getData() {
+    this.url = "http://localhost:3000/countries";
+    return this.http.get<any[]>(this.url);
+  }
+
+  loadCountries(){
+
+    this.getData().subscribe(country => {
+      this.countries = [...country];
+      console.log(this.countries)
+    });
+
   }
 
   // latitude: number;
